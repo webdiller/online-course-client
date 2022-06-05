d90d64fe4d1d28b6bcd43313cbb661fdcc99f63f
eugene <eugenefromrus@gmail.com>
2022-06-05 17:15:24 +1000

    Вернул ендпоинты

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = withBundleAnalyzer({
   images: {
-    domains: ['loremflickr.com']
+    domains: ['loremflickr.com', 'ibb.co']
   },
   env: {
     // FIXME: Расскоментировать при проде
-    // HOST: process.env.NODE_ENV === 'production' ? 'https://online-courses.vercel.app/' : 'http://localhost:3001',
-    // API: process.env.NODE_ENV === 'production' ? 'https://online-courses.vercel.app/api/v1' : 'http://localhost:3001/api/v1',
+    // HOST: process.env.NODE_ENV === 'production' ? 'http://localhost:3001' : 'http://localhost:3001',
+    // API: process.env.NODE_ENV === 'production' ? 'http://localhost:3001/api/v1' : 'http://localhost:3001/api/v1',
 
     // FIXME: Удалить при проде
-    HOST: process.env.NODE_ENV === 'production' ? 'http://5.63.155.52:3001' : 'http://5.63.155.52:3001',
-    API: process.env.NODE_ENV === 'production' ? 'http://5.63.155.52:3001/api/v1' : 'http://5.63.155.52:3001/api/v1',
+    HOST: process.env.NODE_ENV === 'production' ? 'http://localhost:3001' : 'http://localhost:3001',
+    API: process.env.NODE_ENV === 'production' ? 'http://localhost:3001/api/v1' : 'http://localhost:3001/api/v1',
   },
   reactStrictMode: false,
   swcMinify: true,
